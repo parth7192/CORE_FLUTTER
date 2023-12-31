094cdad445e8ecbc41ad2dc2a75ebf95853295bb
parth7192 <142138451+parth7192@users.noreply.github.com>
2023-12-31 13:16:27 +0530

    Update Q_3.dart

--- Q_3.dart
+++ Q_3.dart
@@ -1,94 +1,58 @@
 import 'dart:io';
 
 void main() {
-  List<int> array = [];
- {
-    print("\nMenu:");
-    print("1. Insert element");
-    print("2. Update element");
-    print("3. Delete element");
-    print("4. View elements");
-    print("5. Exit");
+  stdout.write("Enter size of in Array : ");
+  int size = int.parse(stdin.readLineSync()!);
 
-    stdout.write("Enter your choice (1-5): ");
-    String choice = stdin.readLineSync() ?? '';
+  List<int> a = List.generate(size, (index) => index);
 
-    switch (choice) {
-      case '1':
-        insertElement(array);
-        break;
-      case '2':
-        updateElement(array);
-        break;
-      case '3':
-        deleteElement(array);
-        break;
-      case '4':
-        viewElements(array);
-        break;
-      case '5':
-        print("Exiting the program");
-        return;
-      default:
-        print("Invalid choice");
-    }
-  }
-}
-
-void insertElement(List<int> array) {
-  stdout.write("Enter the element to insert: ");
-  int element = int.parse(stdin.readLineSync() ?? '0');
-  array.add(element);
-  print("Element $element inserted successfully.");
-}
-
-void updateElement(List<int> array) {
-  if (array.isEmpty) {
-    print("Array is empty. Cannot perform update operation.");
-    return;
+  for (var i = 0; i < size; i++) {
+    stdout.write("Enter element of $i : ");
+    int val = int.parse(stdin.readLineSync()!);
+    a[i] = val;
   }
 
-  viewElements(array);
+  print("\nOperations :2");
+  print("==============");
+  print("1. Insert");
+  print("2. Delet");
+  print("3. Update");
+  print("4. View");
+  print("5. Exit");
+  print("==============");
+  stdout.write("Enter your choice : ");
+  int choice = int.parse(stdin.readLineSync()!);
 
-  stdout.write("Enter the index to update (0-${array.length - 1}): ");
-  int index = int.parse(stdin.readLineSync() ?? '0');
+  switch (choice) {
+    case 1:
+      stdout.write('Enter The value in insert :');
+      int i = int.parse(stdin.readLineSync()!);
+      stdout.write('Enter value : ');
+      choice = int.parse(stdin.readLineSync()!);
 
-  if (index < 0 || index >= array.length) {
-    print("Invalid index. Please enter a valid index.");
-    return;
-  }
+      a.insert(i, choice);
+      break;
 
-  stdout.write("Enter the new value: ");
-  int newValue = int.parse(stdin.readLineSync() ?? '0');
+    case 2:
+      stdout.write('Enter The value for remove : ');
+      choice = int.parse(stdin.readLineSync()!);
 
-  array[index] = newValue;
-  print("Element at index $index updated to $newValue successfully.");
-}
+      a.remove(choice);
+      break;
 
-void deleteElement(List<int> array) {
-  if (array.isEmpty) {
-    print("Array is empty. Cannot perform delete operation.");
-    return;
-  }
+    case 3:
+      stdout.write('Enter The index of update value : ');
+      int i = int.parse(stdin.readLineSync()!);
+      stdout.write('Enter value : ');
+      choice = int.parse(stdin.readLineSync()!);
 
-  viewElements(array);
+      a[i] = choice;
+      break;
 
-  stdout.write("Enter the index to delete (0-${array.length - 1}): ");
-  int index = int.parse(stdin.readLineSync() ?? '0');
+    case 4:
+      print('List : $a');
 
-  if (index < 0 || index >= array.length) {
-    print("Invalid index. Please enter a valid index.");
-    return;
-  }
-
-  int deletedElement = array.removeAt(index);
-  print("Element at index $index ($deletedElement) deleted successfully.");
-}
-
-void viewElements(List<int> array) {
-  if (array.isEmpty) {
-    print("Array is empty.");
-  } else {
-    print("Elements in the array: $array");
+    default:
+      print('Invalid choice!!');
   }
 }
