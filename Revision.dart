ee7e56e5145a040a8808b2bf60cc1259dd3b8bae
parth7192 <142138451+parth7192@users.noreply.github.com>
2024-01-01 12:58:05 +0530

    Create Revision.dart

---
+++ Revision.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  int l;
+
+  stdout.write("Enter length : ");
+  l = int.parse(stdin.readLineSync() ?? "0");
+
+  List<int> myList = List.generate(l, (index) {
+    stdout.write("$(index + 1}) Enter the value: ");
+    return int.parse(stdin.readLineSync() ?? "0");
+  });
+  myList.sort();
+  print("List: $myList");
+}
