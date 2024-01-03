8c39b1337f108967a003953f5f0abe9d94291ee2
parth7192 <142138451+parth7192@users.noreply.github.com>
2024-01-03 13:02:09 +0530

    Create userinput.dart

---
+++ userinput.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  int id;
+
+  print("Enter your ID : ");
+  id = int.parse(stdin.readLineSync() ?? "0");
+
+  int name;
+
+  print("Enter your Name : ");
+  name = int.parse(stdin.readLineSync() ?? "0");
+
+  int age;
+
+  print("Enter your Age : ");
+  age = int.parse(stdin.readLineSync() ?? "0");
+
+  
+}
